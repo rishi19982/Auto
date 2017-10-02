fb773e7e0850dffa328ff2bb7dacfa7683b8f4fa
Rishabh <bansalrishabh1998@gmail.com>
2017-10-02 13:06:01 +0530

    initial

--- app/src/main/java/com/example/dell/auto/Auto.java
+++ app/src/main/java/com/example/dell/auto/Auto.java
@@ -1,13 +1,52 @@
 package com.example.dell.auto;
 
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Auto extends AppCompatActivity {
-
+    TrieNode tree=new TrieNode();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auto);
+        AssetManager assetManager = getAssets();
+        try {
+            InputStream inputStream = assetManager.open("word_freq.txt");
+            BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+            String line = null;
+            while((line = in.readLine()) != null) {
+                String word = line.trim();
+
+                String[] s=word.split("\\s");
+
+                    tree.addWord(s[0], Long.valueOf(s[1]));
+                    Log.d("Rishabh",Long.valueOf(s[1])+"");
+                    Log.d("Rishabh",s[0]+"|"+s[1]);
+
+
+                  //  Log.d("Rishabh","Failed");
+
+            }
+        } catch (IOException e) {
+            Toast toast = Toast.makeText(this, "Could not load dictionary", Toast.LENGTH_LONG);
+            toast.show();
+
+        }
+        ArrayList<WordClass> Ans=new ArrayList<>();
+        Ans=tree.getPossibleWords("th");
+        for(int k=0;k<Ans.size();k++)
+        {
+            Log.d("Rishabh",Ans.get(k).string);
+        }
+
     }
 }
