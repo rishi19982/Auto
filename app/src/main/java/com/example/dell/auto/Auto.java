398e47ab97ac278d7791abd67b83da91cf8bdb44
AmeyaDaddikar <ameyadaddikar@gmail.com>
2017-10-03 13:54:49 +0530

    Hopefully this one goes in testbranch

--- app/src/main/java/com/example/dell/auto/Auto.java
+++ app/src/main/java/com/example/dell/auto/Auto.java
@@ -22,11 +22,13 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Stack;
 
 public class Auto extends AppCompatActivity {
     TrieNode tree=new TrieNode();
    // private TextView final_text=(TextView)findViewById(R.id.Welcome);
     private String e_text;
+    private Stack<MemoryState> state_stack;
     private TextView edit_text;
     private TextView final_text;
     private TextView[] textView_arr;
@@ -36,6 +38,8 @@ public class Auto extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auto);
+
+        state_stack = new Stack<>();
 
         final_text = (TextView)findViewById(R.id.Welcome);
 
@@ -88,7 +92,10 @@ public class Auto extends AppCompatActivity {
         if(Keycode == KeyEvent.KEYCODE_SPACE || Keycode == KeyEvent.KEYCODE_ENTER ) {
 
             char ws = (char) event.getUnicodeChar();
-            final_text.setText(final_text.getText().toString() + edit_text.getText().toString() + String.valueOf(ws));
+            state_stack.push(new MemoryState(final_text.getText().toString(),edit_text.getText().toString()));
+
+            String new_txt = final_text.getText().toString() + edit_text.getText().toString() + String.valueOf(ws);
+            final_text.setText(new_txt);
             edit_text.setText("");
             for (int i=0;i<5;i++)
             {
@@ -133,9 +140,9 @@ public class Auto extends AppCompatActivity {
             }
 
         }
-       else {
+       //else {
 
-            String input=edit_text.getText().toString();
+            /*String input=edit_text.getText().toString();
             input += (char)event.getUnicodeChar();
             edit_text.setText(input);
             Ans=tree.getPossibleWords(input);
@@ -157,21 +164,26 @@ public class Auto extends AppCompatActivity {
                 }
 
 
-            }
+            }*/
             else{
+                if(!state_stack.empty()) {
+                    MemoryState prev_state = state_stack.pop();
+                    final_text.setText(prev_state.final_string);
+                    edit_text.setText(prev_state.editing_string);
+                }
                 for (int i=0;i<5;i++)
                 {
                     textView_arr[i].setText("");
                 }
             }
-        }
+        //}
        // final_text.setText("Hello");
         return super.onKeyUp(Keycode,event);
     }
 
     public void setText(View view){
         TextView selected_view = (TextView)view;
-
+        state_stack.push(new MemoryState(final_text.getText().toString(),selected_view.getText().toString()));
         final_text.setText(final_text.getText().toString() + selected_view.getText().toString() +" ");
         edit_text.setText("");
 
---
+++ app/src/main/java/com/example/dell/auto/MemoryState.java
@@ -0,0 +1,15 @@
+package com.example.dell.auto;
+
+/**
+ * Created by Sunil on 03-10-2017.
+ */
+
+public class MemoryState {
+    String final_string;
+    String editing_string;
+
+    public MemoryState(String final_string, String editing_string){
+        this.final_string = final_string;
+        this.editing_string = editing_string;
+    }
+}
